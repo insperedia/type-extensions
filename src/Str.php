1a11dcf236ba93f00e94eea8e162ca5cd3ab99b6
oleg <oleg@insperedia.com>
2016-04-25 14:11:13 +0200

    Readme

--- src/Str.php
+++ src/Str.php
@@ -6,9 +6,14 @@ class Str
 {
     protected $string;
 
-    public function __constructor($str)
+    public function __construct($str)
     {
         $this->string = $str;
+    }
+
+    public function __toString()
+    {
+       return $this->string;
     }
 
     public static function n($str)
@@ -17,11 +22,13 @@ class Str
     }
 
     public function trim() {
-        return trim($this->string);
+        $this->string = trim($this->string);
+        return $this;
     }
 
     public function sub($start, $length = null) {
-        return substr($this->string, $start, $length);
+        $this->string = substr($this->string, $start, $length);
+        return $this;
     }
 
     public function find($regex) {
@@ -40,5 +47,30 @@ class Str
 
     public function icontains($str) {
         return stripos($this->string, $str) !== FALSE;
+    }
+
+    public function words() {
+        $words = explode(' ', $this->string);
+        $words = array_filter($words);
+        return \SplFixedArray::fromArray($words);
+    }
+
+    public function split($separator = ' ') {
+        return  explode($separator, $this->string);
+    }
+
+    public function urlencode() {
+        $this->string = urlencode($this->string);
+        return $this;
+    }
+
+    public function replace($search = "\n", $replace= " ") {
+        $this->string = str_replace($this->string, $search, $replace);
+        return $this;
+    }
+
+    public function join() {
+        $this->string = $this->string.implode('', func_get_args());
+        return $this;
     }
 }
